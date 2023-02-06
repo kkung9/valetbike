e7fb2ff2bf68dbf2de68470567e57e5ec92bfd14
Karenna Kung <kkung@smith.edu>
2023-02-05 20:50:08 -0500

    A1 stations and bikes lists

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Station.create!([{
+#   identifier: 21,
+#   name: "Florence Bank Station",
+#   address: "19 Meadow Street Florence MA @ Lilly Library"
+# }])
+
+# p "Created station"
+
+Station.destroy_all
+
+require 'csv'
+csv_text = File.read(Rails.root.join('notes', 'station-data.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+    s = Station.new
+    s.identifier = row['identifier']
+    s.name = row['name']
+    s.address = row['address']
+    s.save
+    puts "#{s.name} saved"
+end
+puts "There are now #{Station.count} rows in the stations table"
+
+Bike.destroy_all
+
+require 'csv'
+csv_text = File.read(Rails.root.join('notes', 'bike-data.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+    b = Bike.new
+    b.identifier = row['identifier']
+    b.current_station_id = row['current_station_identifier']
+    s = Station.find_by(identifier: b.current_station_id)
+    b.current_station = s
+    b.save
+    puts "#{b.identifier} saved"
+end
+puts "There are now #{Bike.count} rows in the bikes table"
