23c46bca35aa08aeefbbee4ef99e7a2d94869f25
Audrey Kim <akim89@smith.edu>
2023-03-29 16:58:14 -0400

    Renaming column names (camel -> underscores)

--- db/migrate/20230321213551_create_rentals.rb
+++ db/migrate/20230321213551_create_rentals.rb
@@ -1,11 +1,11 @@
 class CreateRentals < ActiveRecord::Migration[7.0]
   def change
     create_table :rentals do |t|
-      t.datetime :startTime
-      t.datetime :predictedEndTime
-      t.datetime :actualEndTime
-      t.float :predictedFee
-      t.float :actualFee
+      t.datetime :start_time
+      t.datetime :predicted_end_time
+      t.datetime :actual_end_time
+      t.float :predicted_fee
+      t.float :actual_fee
 
       t.timestamps
     end
