205d5a923ed3b458955f7ea4c4b936cdd09d5402
Audrey Kim <akim89@smith.edu>
2023-03-31 00:12:41 -0400

    adding lat and long to multiple places yay

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_26_235702) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_31_032446) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "dock_id"
@@ -55,6 +55,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_26_235702) do
     t.datetime "updated_at", null: false
     t.string "photo"
     t.string "description"
+    t.float "lat"
+    t.float "long"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
