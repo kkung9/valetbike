6d0a43d33f6fb03478098fa3b36957d68f8f5f64
HillaryDunkley <hvgdunkley@gmail.com>
2023-04-05 19:29:31 -0400

    commit message here

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -5,7 +5,10 @@ class BikesController < ApplicationController
     else
       @bikes = Bike.all.order(identifier: :desc)
 
-      @current_user = Bike.find(3).rental.where(is_complete: false).first.user  
+      @current_user = Bike.all.where(identifier: 1000).first.rental.where(is_complete: false).first.user.firstName 
+      puts(@current_user)
+
+      @past_users = Bike.all.where(identifier: 1000).first.user
       # replace 3 with params for applicability
       # @current_user_id = @current_user.first.user_id
 
