437e513d5363a0f77c24e0fe0ac73fad315262ec
yurika <yurimocha02@gmail.com>
2023-04-20 20:01:24 -0400

    adds validations to ensure that bikes cannot be in both rental and dock

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,5 +1,15 @@
 class Rental < ApplicationRecord
     belongs_to :user, optional: true
     belongs_to :bike, optional: true
+
+    validate :bike_is_not_in_both_active_rental_and_dock
+
+    private
+
+    def bike_is_not_in_both_active_rental_and_dock
+      if bike.rental.where(is_complete: nil).count >= 1 && bike.dock.present?
+        errors.add(:description, "cannot be in both active rental and dock")
+      end
+    end
     
 end
