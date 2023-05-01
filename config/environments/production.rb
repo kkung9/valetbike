7c98beb71a1f15ca8befc40ba00ca732ebce4c7e
Karenna Kung <kkung@smith.edu>
2023-04-30 20:21:24 -0400

    change mailer password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,7 +101,7 @@ Rails.application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :user_name            => "valetbike223@gmail.com",
-    :password             => "RubyRenegades223",
+    :password             => "uorzrzahaqnptvbk",
     :authentication       => "plain",
     :enable_starttls_auto => true
     }
